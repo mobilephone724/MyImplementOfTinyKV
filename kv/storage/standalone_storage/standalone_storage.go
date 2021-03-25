cb6ba0b8697a6ca408785200208b4b01b0308dc6
mobilephone724 <1263485219@qq.com>
2021-03-25 14:35:34 +0800

    a little implement of project1

---
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -0,0 +1,61 @@
+package standalone_storage
+
+import (
+	"log"
+
+	"github.com/Connor1996/badger"
+	"github.com/pingcap-incubator/tinykv/kv/config"
+	"github.com/pingcap-incubator/tinykv/kv/storage"
+	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
+)
+
+// StandAloneStorage is an implementation of `Storage` for a single-node TinyKV instance. It does not
+// communicate with other nodes and all data is stored locally.
+type StandAloneStorage struct {
+	StandAloneDb *badger.DB
+	conf         *config.Config
+	// Your Data Here (1).
+}
+
+type StandAloneStorageReader struct {
+}
+
+func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
+	// Your Code Here (1).
+	return &StandAloneStorage{
+		StandAloneDb: nil,
+		conf:         conf,
+	}
+	// return nil
+}
+
+func (s *StandAloneStorage) Start() error {
+	// Your Code Here (1).
+
+	opt := badger.DefaultOptions
+	opt.Dir = s.conf.DBPath
+	opt.ValueDir = s.conf.DBPath
+	var err error
+	s.StandAloneDb, err = badger.Open(opt)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	return nil
+}
+
+func (s *StandAloneStorage) Stop() error {
+	// Your Code Here (1).
+	return s.StandAloneDb.Close()
+	// return nil
+}
+
+func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
+	// Your Code Here (1).
+	return nil, nil
+}
+
+func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
+	// Your Code Here (1).
+	return nil
+}
